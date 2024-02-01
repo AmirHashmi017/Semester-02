55959e765984bd3fdb6b74f0cfb78dc83ede59c2
AMIR HASHMI <144407626+AmirHashmi017@users.noreply.github.com>
2024-02-01 20:02:51 +0500

    Add files via upload

---
+++ OOP LAB/Week3/Challenge2 (Class).cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Lab2_Task2
+{
+    internal class Product
+    {
+        public string ProductID;
+        public string name;
+        public float price;
+        public string category;
+        public string brand;
+        public string country;
+        public Product(string id, string name, float price, string category, string brand, string country)
+        {
+            this.ProductID = id;
+            this.name = name;
+            this.price = price;
+            this.category = category;
+            this.brand = brand;
+            this.country = country;
+        }
+public void showproducts()
+        {
+Console.WriteLine("{0} \t\t\t {1} \t\t  {2} \t\t {3}\t\t\t{4}\t\t{5}",ProductID, name, price,category, brand, country);
+	}
+    }
+}
+
---
+++ OOP LAB/Week3/Self Assessement2(Class).cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfAssessement2
+{
+    internal class ATMaccount
+    {
+        public int balance;
+        public List<string> transactionrecords = new List<string>();
+        public ATMaccount(int defaultamount)
+        {
+            this.balance = defaultamount;
+        }
+        public void depositmoney(int amount)
+        {
+            balance = balance + amount;
+            transactionrecords.Add("Deposited: " + amount);
+        }
+        public void withdrawmoney(int amount)
+        {
+            balance = balance - amount;
+            transactionrecords.Add("Withdrawn: " + amount);
+        }
+        public void viewbalance()
+        {
+            Console.Write("Your current balance is: {0}\n", balance);
+        }
+        public void viewtransactionrecords()
+        {
+            for (int i = 0; i < transactionrecords.Count; i++)
+            {
+                Console.WriteLine(transactionrecords[i]);
+            }
+        }
+    }
+}
