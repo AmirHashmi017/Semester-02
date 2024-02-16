a4fd7b1d471c96752214e413cfc4947169795ea5
AMIR HASHMI <144407626+AmirHashmi017@users.noreply.github.com>
2024-02-16 19:13:10 +0500

    Add files via upload

---
+++ OOP LAB/Week 4/Task 2 (Book Class ).cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    internal class Book
+    {
+        public string title;
+        public List<string> author = new List<string>();
+        public int publicationyear;
+        public float price;
+        public int stock;
+        public string publisher;
+        public string ISBN;
+        public Book(string title, List<string> authors, int year, float price, int quantity, string publisher, string ISBN)
+        {
+            this.title = title;
+            for (int i = 0; i < authors.Count; i++)
+            {
+                author.Add(authors[i]);
+            }
+            this.publicationyear = year;
+            this.price = price;
+            this.stock = quantity;
+            this.publisher = publisher;
+            this.ISBN = ISBN;
+        }
+        public string bookDetails()
+        {
+            return $" {title} \t {ISBN} \t   {publisher} \t\t {publicationyear} \t\t {price} \t  {stock} ";
+        }
+        public bool increasestock(int number)
+        {
+            stock = stock + number;
+            return true;
+        }
+        public bool decreasestock(int number)
+        {
+            if (stock >= number)
+            {
+                stock = stock - number;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ OOP LAB/Week 4/Task 2 (Member Class ).cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    internal class Member
+    {
+        public string membername;
+        public string memberID;
+        public List<Book> booksbought = new List<Book>();
+        public int numberofboughtbooks;
+        public double balance;
+        public double amountspent;
+        public Member(string membername, string memberID, int number, float balance, float amountspent)
+        {
+            this.membername = membername;
+            this.memberID = memberID;
+            this.numberofboughtbooks = number;
+            this.balance = balance;
+            this.amountspent = amountspent;
+        }
+        public string memberDetails()
+        {
+            return $" {membername} \t {memberID} \t\t {numberofboughtbooks} \t\t {balance} \t\t\t {amountspent}";
+        }
+        public bool editmember(string name, string ID)
+        {
+            membername = name;
+            memberID = ID;
+            return true;
+        }
+        public bool purchasebooks(Book purchasedbook, int qty)
+        {
+            if (qty <= purchasedbook.stock)
+            {
+                double discountedprice = 0;
+                double total = 0;
+                if (memberID != "0")
+                {
+                    discountedprice = purchasedbook.price - (purchasedbook.price * 0.05);
+                    total = discountedprice * qty;
+                    if (total <= balance)
+                    {
+                        booksbought.Add(purchasedbook);
+                        numberofboughtbooks += qty;
+                        balance -= total;
+                        amountspent += total;
+                        purchasedbook.stock -= qty;
+                        return true;
+                    }
+                        return false;
+                }
+                else if (memberID == "0")
+                {
+                    total = purchasedbook.price * qty;
+                    if (total <= balance)
+                    {
+                        booksbought.Add(purchasedbook);
+                        numberofboughtbooks += qty;
+                        balance -= total;
+                        amountspent += total;
+                        purchasedbook.stock -= qty;
+                        return true;
+                    }
+                        return false;
+                }
+            }
+            return false;
+        }
+    }
+}
+
