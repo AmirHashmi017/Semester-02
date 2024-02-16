ab5ff044073a2cb2007d2d6537c32c1d16a3f770
AMIR HASHMI <144407626+AmirHashmi017@users.noreply.github.com>
2024-02-16 19:10:49 +0500

    Add files via upload

---
+++ OOP PD/Week 4/Task 1(Angle Class).cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_Week4_
+{
+    internal class Angle
+    {
+        public int degree;
+        public float minutes;
+        public string direction;
+
+        public Angle(int degree, float minutes, string direction)
+        {
+            this.degree = degree;
+            this.minutes = minutes;
+            this.direction = direction;
+        }
+    }
+}
