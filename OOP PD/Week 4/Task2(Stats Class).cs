3f13aa1f3aa9edf01d009ecf5e019de83d98a364
AMIR HASHMI <144407626+AmirHashmi017@users.noreply.github.com>
2024-02-17 18:04:46 +0500

    Add files via upload

---
+++ OOP PD/Week 4/Task2(Stats Class).cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    internal class Stats
+    {
+        public string skillname;
+        public double damage;
+        public double penetration;
+        public double cost;
+        public double heal;
+        public string description;
+
+        public Stats(string skillname, double damage, double penetration, double cost, double heal, string description)
+        {
+            this.skillname = skillname;
+            this.damage = damage;
+            this.penetration = penetration;
+            this.cost = cost;
+            this.heal = heal;
+            this.description = description;
+        }
+    }
+}
