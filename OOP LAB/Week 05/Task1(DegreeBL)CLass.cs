282de9afcdd1126b8c169aa2dbcdbf70e8bd8b64
AMIR HASHMI <144407626+AmirHashmi017@users.noreply.github.com>
2024-02-20 19:17:05 +0500

    Add files via upload

---
+++ OOP LAB/Week 05/Task1(DegreeBL)CLass.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Task1_Lab_5_
+{
+    internal class Degree
+    {
+        public string Degreetitle;
+        public int Duration;
+        public int Seats;
+        List<Subject> Subjectsadd = new List<Subject>();
+
+        public Degree(string degreetitle, int duration, int seats)
+        {
+            this.Degreetitle = degreetitle;
+            this.Duration = duration;
+            this.Seats = seats;
+        }
+
+        public bool addsubject(Subject sub)
+        {
+            int totalcredithours = calculatecredithours() + sub.Credithours;
+            if (totalcredithours <= 20)
+            {
+                Subjectsadd.Add(sub);
+                return false;
+            }
+            return true;
+        }
+        public bool issubjectexist(Subject sub)
+        {
+            for (int i = 0; i < Subjectsadd.Count; i++)
+            {
+                if (Subjectsadd[i].Subjectcode == sub.Subjectcode)
+                    return true;
+            }
+            return false;
+        }
+        public int calculatecredithours()
+        {
+            int hours = 0;
+            for (int i = 0; i < Subjectsadd.Count; i++)
+            {
+                hours = hours + Subjectsadd[i].Credithours;
+            }
+            return hours;
+        }
+    }
+}
---
+++ OOP LAB/Week 05/Task1(DegreeDL)Class.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_Lab_5_
+{
+    internal class DegreeDL
+    {
+        static public List<Degree> Degrees = new List<Degree>();
+        
+        public static void AddDegree(Degree degree)
+        {
+        Degrees.Add(degree);
+        }
+    }
+}
+
