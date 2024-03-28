6f89e6e11d6d32189c9c158ca11a86c91c006733
AMIR HASHMI <144407626+AmirHashmi017@users.noreply.github.com>
2024-03-28 14:21:44 +0500

    Add files via upload

---
+++ OOP LAB/Week 09/Task 03(Color Challange)/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace colorChallange
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Color> colors = new List<Color>() { Color.Red,Color.Green,Color.Blue};
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int index = rnd.Next(colors.Count);
+          
+           textBox1.BackColor = colors[index];
+            
+
+
+        }
+    }
+}
